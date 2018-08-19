8901549e90882d8cac0d95b131e1ff7dc8e8fca5
Paulo de Oliveira <paulo-gyun@outlook.com>
2018-08-19 19:52:53 -0300

    docs: Create initial read me
    Create initial read me of the project with instructions to setup and run
    the project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Button, Container, Menu} from 'semantic-ui-react'
 import './App.css';
+import * as BooksAPI from './utils/BooksAPI'
+import * as ArrayUtil from './utils/ArrayUtil'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    state = {
+        books: []
+    };
+
+    componentDidMount() {
+        const getBooksByShelf = item => {
+            return item.shelf;
+        };
+
+        BooksAPI.getAll().then(allBooks => {
+            const books = ArrayUtil.groupBy(allBooks, getBooksByShelf);
+            this.setState({ books });
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+
+                <Menu className="App-bar">
+                    <div className="App-title">My reads</div>
+                </Menu>
+
+                <Container>
+                    <Button
+                        content='Discover docs'
+                        href='https://react.semantic-ui.com'
+                        icon='github'
+                        labelPosition='left'
+                    />
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default App;
