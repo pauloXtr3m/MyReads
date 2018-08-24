599ff53598df25fa326fe60d185781a9d764a85e
Paulo de Oliveira Neto <pauloneto@megasoftgyn.com.br>
2018-08-24 14:21:44 -0300

    feat: Add right click menu
    Add right click menu to change book book shelf

--- src/Book.js
+++ src/Book.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 import { Item } from 'semantic-ui-react'
+import { ContextMenuProvider } from 'react-contexify'
+import RightClickMenu from "./RightClickMenu";
 
 class Book extends Component{
 	render(){
-		const data = this.props.data;
+		const { data, onChange } = this.props;
+		const contextMenuId = 'menu_id'+ data.id;
 
 		return (
 			<div className='book'>
-				<Item>
-					<Item.Content>
-						<Item.Image className='book-image' size='small' src={data.imageLinks.smallThumbnail}/>
-						<Item.Description>{data.title}</Item.Description>
-					</Item.Content>
-				</Item>
+                <ContextMenuProvider id={contextMenuId} >
+                    <Item>
+                        <Item.Content>
+                            <Item.Image className='book-image' size='small' src={data.imageLinks.smallThumbnail}/>
+                            <Item.Description>{data.title}</Item.Description>
+                        </Item.Content>
+                    </Item>
+                </ContextMenuProvider>
+				<RightClickMenu id={contextMenuId}/>
 			</div>
 		);
 	}
